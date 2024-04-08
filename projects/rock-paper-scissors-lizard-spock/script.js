33038626dfe961959142e4c68ea62e239066d02a
annafkt <44895565+annafkt@users.noreply.github.com>
2024-04-08 17:55:25 +0200

    Make changes for another pop-up
    Made the rules section's code more general
    in order to be able to add another pop-up later
    (for displaying the final result).

--- projects/rock-paper-scissors-lizard-spock/script.js
+++ projects/rock-paper-scissors-lizard-spock/script.js
@@ -1,12 +1,24 @@
+const popUps = document.querySelectorAll('.pop-up');
+const xMarks = document.querySelectorAll('.x-mark-wrapper');
+
 /*
-Reveal and hide rules
+Manage pop-ups
 *********************/
 
 document.querySelector('.rules-button-wrapper').addEventListener('click', () => {
   document.documentElement.scrollTo({top: 0, behavior: 'smooth'});
-  document.querySelector('.rules-container').classList.add('reveal');
+
+  popUps.forEach(popUp => {
+    if (popUp.classList.contains('rules-container')) {
+      popUp.classList.add('reveal');
+    }
+  });
 });
 
-document.querySelector('.x-mark-wrapper').addEventListener('click', () => {
-  document.querySelector('.rules-container').classList.remove('reveal');
+xMarks.forEach(xMark => {
+  xMark.addEventListener('click', () => {
+    popUps.forEach(popUp => {
+      popUp.classList.remove('reveal');
+    });
+  });
 });
