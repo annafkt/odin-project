a22609b136aebe4367c43132baf7f3b1c5f6d1bb
annafkt <44895565+annafkt@users.noreply.github.com>
2024-04-10 14:52:32 +0200

    Add game logic

--- projects/rock-paper-scissors-lizard-spock/script.js
+++ projects/rock-paper-scissors-lizard-spock/script.js
@@ -1,5 +1,23 @@
+/*
+Rules: object properties beat their value 
+*****************************************/
+const RULES = {
+
+  'rock': ['lizard', 'scissors'],
+  'paper': ['rock', 'spock'],
+  'scissors': ['paper', 'lizard'],
+  'lizard': ['spock', 'paper'],
+  'spock': ['scissors', 'rock']
+
+};
+
 const popUps = document.querySelectorAll('.pop-up');
 const xMarks = document.querySelectorAll('.x-mark-wrapper');
+
+const handSignals = document.querySelectorAll('.hand-signals .wrapper');
+
+let playerPoints = 0;
+let computerPoints = 0;
 
 /*
 Manage pop-ups
@@ -22,3 +40,65 @@ xMarks.forEach(xMark => {
     });
   });
 });
+
+/*
+Game logic
+**********/
+
+handSignals.forEach(signal => {
+  signal.addEventListener('click', (e) => {
+    const playerChoice = e.currentTarget.firstElementChild.id;
+    const computerChoice = getComputerChoice();
+
+    addPoints(playerChoice, computerChoice);
+  });
+});
+
+function getComputerChoice() {
+  const options = Object.keys(RULES);
+
+  return options[Math.floor(Math.random() * options.length)];
+}
+
+function addPoints(playerChoice, computerChoice) {
+  const playerScore = document.querySelector('.score .player p');
+  const computerScore = document.querySelector('.score .computer p');
+
+  if (playerChoice == computerChoice) {
+    playerPoints += 1;
+    computerPoints += 1;
+  } else if (RULES[playerChoice].includes(computerChoice)) {
+    playerPoints += 1;
+  } else {
+    computerPoints += 1;
+  }
+
+  playerScore.textContent = playerPoints;
+  computerScore.textContent = computerPoints;
+
+  if (playerPoints == 5 || computerPoints == 5) {
+    displayFinalResult();
+
+    playerPoints = 0;
+    computerPoints = 0;
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+  }
+}
+
+function displayFinalResult() {
+  const resultCard = document.querySelector('.final-result-container');
+  const para = document.querySelector('.final-result-container p');
+
+  if (playerPoints > computerPoints) {
+    para.textContent = 'You won!';
+  } else if (playerPoints < computerPoints) {
+    para.textContent = 'You lost!';
+  } else {
+    para.textContent = 'The game ended in a tie.';
+  }
+  
+  document.documentElement.scrollTo({top: 0, behavior: 'smooth'});
+
+  resultCard.classList.add('reveal');
+}
